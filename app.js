f12f0b5e046577e098faaddb4dfc318abaf57c19
kungbaman <natchapol.kamo@hotmail.com>
2022-01-03 20:14:59 +0700

    a little update

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const empty = document.querySelector('.empty_room');
+const empty2 = document.querySelector('.empty_room2');
+const empty3 = document.querySelector('.empty_room3');
+
+empty.addEventListener('click',messages);
+empty2.addEventListener('click',messages);
+empty3.addEventListener('click',messages);
+
+
 function messages(){
     alert("ห้องว่าง !!");
 }
